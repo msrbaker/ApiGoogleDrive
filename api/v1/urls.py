d83add51df6fb3afb772b184a8068ad891661be5
HacKan <hackan@gmail.com>
2019-01-12 20:35:02 -0300

    Create /search-in-doc API endpoint

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -7,10 +7,11 @@ from . import views
 app_name = 'v1'
 
 router = routers.DefaultRouter()
-
 router.register('file', views.FileViewSet)
 
 urlpatterns = [
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include(router.urls)),
+    path('search-in-doc/', views.SearchInDocView.as_view()),
+    path('search-in-doc/<str:file_id>', views.SearchInDocView.as_view(), name='search'),
 ]
--- api/v1/views.py
+++ api/v1/views.py
@@ -1,10 +1,48 @@
-from rest_framework import viewsets
+from oauth2client import file, client, tools
+from googleapiclient.discovery import build
+from httplib2 import Http
+
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
+from rest_framework import views, viewsets
 from rest_framework import status
+from django.conf import settings
 
 from common.models import File
 from api.v1 import serializers
+
+
+class SearchInDocView(views.APIView):
+    SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+
+    def get(self, request, file_id=None):
+        word = request.GET.get('word')
+        if file_id and word:
+            store = file.Storage('token.json')
+            creds = store.get()
+            if not creds or creds.invalid:
+                flow = client.flow_from_clientsecrets(
+                    settings.GDRIVE_CREDS_OAUTH_FILE,
+                    self.SCOPES
+                )
+                creds = tools.run_flow(flow, store)
+            service = build('drive', 'v3', http=creds.authorize(Http()))
+
+            # Call the Drive v3 API
+            # https://developers.google.com/drive/api/v3/reference/files/list
+            # https://developers.google.com/drive/api/v3/search-parameters
+            results = service.files().list(
+                fields="nextPageToken, files(id, name)",
+                q="fullText contains '{}'".format(word)
+            ).execute()
+            items = results.get('files', [])
+            for item in items:
+                if item['id'] == file_id:
+                    return Response(item, status.HTTP_200_OK)
+
+            return Response('File not found', status.HTTP_404_NOT_FOUND)
+        return Response('Usage: /<id>?word=<search term>',
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
 class FileViewSet(viewsets.ModelViewSet):
